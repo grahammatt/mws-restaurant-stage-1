2335f3794874f5d13b21a054a670a6da5192d9ad
Matt Graham <grahammatt@live.com>
2018-11-09 00:44:49 -0500

    feat: add service worker

---
+++ scripts/registerSW.js
@@ -0,0 +1,14 @@
+/*Register Service Worker
+ *https://developers.google.com/web/fundamentals/primers/service-workers/
+ */
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js').then(registration => {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, err => {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
---
+++ sw.js
@@ -0,0 +1,39 @@
+/*Service Worker
+ *https://developers.google.com/web/fundamentals/primers/service-workers/
+ */
+const CACHE_NAME = 'my-site-cache-v1';
+let urlsToCache = [
+  '/',
+  '/index.html',
+  '/restaurant.html',
+  '/styles/styles.css',
+  '/scripts/dbHelper.js',
+  '/scripts/restaurant_info.js',
+  '/scripts/main.js',
+  '/data/restaurants.json'
+];
+
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    //open cache
+    caches.open(CACHE_NAME)
+    .then(cache => {
+      console.log('Opened cache');
+      //add to cache
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  //when a url is fetched
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      // Cache hit - return response
+      // else attempt to fetch
+      return response ? response : fetch(event.request);
+    })
+  );
+});
